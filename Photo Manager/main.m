7b5633674ce2ba7b87e0825a424d01bf1aa5ee86
Xintong Song <tonysong820@gmail.com>
2014-02-12 16:17:25 +0800

    Initial Commit

---
+++ Photo Manager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Manager
+//
+//  Created by Tony Song on 14-2-12.
+//  Copyright (c) 2014年 WeeSteps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
---
+++ Photo ManagerTests/Photo_ManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  Photo_ManagerTests.m
+//  Photo ManagerTests
+//
+//  Created by Tony Song on 14-2-12.
+//  Copyright (c) 2014年 WeeSteps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Photo_ManagerTests : XCTestCase
+
+@end
+
+@implementation Photo_ManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
